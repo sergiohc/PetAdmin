cde62d5102e9f27bfa918cd2a1d1ff0fc0ae6c61
sergiohc <sergioh.c@live.com>
2018-08-02 13:22:27 -0300

    call job in campaing

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,9 +3,23 @@ class Campaign < ApplicationRecord
   def fae_display_field
     title
   end
+
   has_many :campaign_clients, dependent: :destroy
   has_many :clients, through: :campaign_clients
 
   validates :title, presence: true
   validates :body, presence: true
+
+  after_save :schedule_emails
+ 
+  def fae_display_field
+    title
+  end
+ 
+  def schedule_emails
+    Client.find_each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
 end
